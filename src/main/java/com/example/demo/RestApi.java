a79eb58ca4731bec2ff6b8f5737d42e11d73146b
PhuongThe12 <ppoloh3@gmail.com>
2023-10-25 18:21:15 +0700

    add download

--- src/main/java/com/example/demo/RestApi.java
+++ src/main/java/com/example/demo/RestApi.java
@@ -1,12 +1,18 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -29,11 +35,11 @@ public class RestApi {
         try {
             InputStream inputStream = file.getInputStream();
             Path uploadPath = Paths.get(uploadDir);
-            if(!Files.exists(uploadPath)){
+            if (!Files.exists(uploadPath)) {
                 Files.createDirectories(uploadPath);
             }
             Path filePath = uploadPath.resolve(file.getOriginalFilename());
-            Files.copy(inputStream,filePath, StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
             return "File uploaded successfully.";
         } catch (IOException e) {
             e.printStackTrace();
@@ -41,4 +47,28 @@ public class RestApi {
         }
     }
 
+    @GetMapping("/files/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName) throws IOException {
+        // Đường dẫn tới thư mục 'static'
+        String staticDirectory = "src/main/resources/static/files";
+
+        // Tạo đường dẫn đến tệp trong thư mục 'static'
+        Path filePath = Paths.get(staticDirectory).resolve(fileName).normalize();
+        Resource resource = new UrlResource(filePath.toUri());
+
+        // Kiểm tra xem tệp có tồn tại không
+        if (resource.exists() && resource.isReadable()) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + resource.getFilename());
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(resource.contentLength())
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .body(resource);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+
+    }
 }
